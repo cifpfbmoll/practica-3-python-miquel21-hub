530323293813315c20ebd4bda40121f20e2ec2a8
miquel21-hub <mribas@cifpfbmoll.eu>
2020-11-16 18:29:40 +0100

    practica 3

--- P3EJ1.py
+++ P3EJ1.py
@@ -1,3 +1,4 @@
 #Realiza un programa que pida al usuario su nombre y despues lo imprima en pantalla.
-NOMBRE=input("Hola, ¿como te llamas?:\n")
+NOMBRE=input("Hola, ¿como te llamas?:\n ")
 print(NOMBRE)
+ 
